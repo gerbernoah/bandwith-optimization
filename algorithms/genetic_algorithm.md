3c33a477b11c786ca4a3fc378521734667c9cd49
Krzysztof Lukasik <krzys01@hotmail.com>
2025-08-03 14:51:04 +0200

    bigRun2

--- algorithms/genetic_algorithm.md
+++ algorithms/genetic_algorithm.md
@@ -3,8 +3,6 @@
 - calculate 5 shortests paths (lowest routing cost) for each demand
 - a DNA in GA consists of |E| integers plus 5*|D| floats 
 - |E| integers represent the chosen module on an edge, and the floats represent the demand flow on a path
-- use about 1000 individuals at first, set upper limit to 5000
-- 
 
 
 ## How GA works
--- main.py
+++ main.py
@@ -28,7 +28,7 @@ print_summary("IP", resultsIP)
 """
 # results = run_GA(network, ngen=50)
 # print_summary("GA", results)
-
+"""
 base_params = GAParams(
     npop=80,
     cxpb=0.6,
@@ -68,3 +68,36 @@ for i in range(len(param_lists)):
 
     plot_parameter_performance(param_lists_names[i], file_names, nruns, resultsIP.total_cost)
     plot_parameter_runtime(param_lists_names[i], file_names, nruns, resultsIP.total_runtime)
+"""
+
+params = GAParams(
+    npop=80,
+    cxpb=0.8,
+    mutpb=0.2,
+    ngen=40,
+    penalty_coeff=100000.0,
+    max_module_ratio=0.4,
+    min_module_ratio=0.2,
+    muLambda=(1, 1.7),
+    indp=0.4,
+    tournsize=3
+)
+
+# number of runs per param value
+# nruns = [18, 19, 20]
+runs = 10
+
+results = []
+runtimes = []
+print("==== running GA analysis ====")
+for j in range(runs):
+    result = run_GA(network, params, False)
+    print(f"Run ({j+1}/{runs}): {result.total_cost}, {result.total_runtime}s")
+    results.append(result.total_cost)
+    runtimes.append(result.total_runtime)
+
+error_percentages = [100 * ((cost - resultsIP.total_cost) / resultsIP.total_cost) for cost in results]
+avgerror = sum(error_percentages) / len(error_percentages)
+avgruntime = sum(runtimes) / len(runtimes)
+
+print(f"\nAverage Results: avgerror = {avgerror}, avgruntime = {avgruntime}s")
